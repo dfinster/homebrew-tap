4ef36dd9df9fd79be62089f102d43b2c3d86d229
GitHub Action <action@github.com>
2025-07-12 23:56:26 +0000

    Update branch-wrangler to v0.0.9

--- Formula/branch-wrangler.rb
+++ Formula/branch-wrangler.rb
@@ -1,42 +1,42 @@
 class BranchWrangler < Formula
   desc "Cross-platform TUI for managing local Git branches with GitHub integration"
   homepage "https://github.com/dfinster/branch-wrangler"
-  version "0.0.8"
+  version "0.0.9"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/dfinster/branch-wrangler/releases/download/v0.0.8/branch-wrangler-v0.0.8-darwin-arm64"
-      sha256 "5488f7d113d8f4121e4f2b0bbadfbdecac8dd2bba9cd3f71da2acf04cf4a7d99"
+      url "https://github.com/dfinster/branch-wrangler/releases/download/v0.0.9/branch-wrangler-v0.0.9-darwin-arm64"
+      sha256 "740e20f1eb3a84f587c1947fd8d7d65f7e3ac33b3fec7576e8364eae5fa5a599"
     end
 
     on_intel do
-      url "https://github.com/dfinster/branch-wrangler/releases/download/v0.0.8/branch-wrangler-v0.0.8-darwin-amd64"
-      sha256 "d25f4d76a2082ee10082b87f35042b58d43012a2a0970ac2ed50be7cbc158a1d"
+      url "https://github.com/dfinster/branch-wrangler/releases/download/v0.0.9/branch-wrangler-v0.0.9-darwin-amd64"
+      sha256 "efdbfddd7a5986ebdf329f3726ff754b985d7259d91b5ec6329bbc46484de729"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/dfinster/branch-wrangler/releases/download/v0.0.8/branch-wrangler-v0.0.8-linux-arm64"
-      sha256 "237a4c854b8c20424eae9c82e129be9caaaf8f631d64a2f744a1c4329d7510d5"
+      url "https://github.com/dfinster/branch-wrangler/releases/download/v0.0.9/branch-wrangler-v0.0.9-linux-arm64"
+      sha256 "658dcacebe0d779981145f9a109039268445783fd277bcb47206a688951b20ad"
     end
 
     on_intel do
-      url "https://github.com/dfinster/branch-wrangler/releases/download/v0.0.8/branch-wrangler-v0.0.8-linux-amd64"
-      sha256 "d5e8a0e95d51c9a5a95ca7f07fe9a6a3b4ebe4fb2d8c8ba1c645b0365a9b3e6b"
+      url "https://github.com/dfinster/branch-wrangler/releases/download/v0.0.9/branch-wrangler-v0.0.9-linux-amd64"
+      sha256 "5627d50fcab686e50798df352fcb5ffda83db411ab0714a6d3acd53e4e82a39c"
     end
   end
 
   def install
     if OS.mac? && Hardware::CPU.arm?
-      bin.install "branch-wrangler-v0.0.8-darwin-arm64" => "branch-wrangler"
+      bin.install "branch-wrangler-v0.0.9-darwin-arm64" => "branch-wrangler"
     elsif OS.mac? && Hardware::CPU.intel?
-      bin.install "branch-wrangler-v0.0.8-darwin-amd64" => "branch-wrangler"
+      bin.install "branch-wrangler-v0.0.9-darwin-amd64" => "branch-wrangler"
     elsif OS.linux? && Hardware::CPU.arm?
-      bin.install "branch-wrangler-v0.0.8-linux-arm64" => "branch-wrangler"
+      bin.install "branch-wrangler-v0.0.9-linux-arm64" => "branch-wrangler"
     elsif OS.linux? && Hardware::CPU.intel?
-      bin.install "branch-wrangler-v0.0.8-linux-amd64" => "branch-wrangler"
+      bin.install "branch-wrangler-v0.0.9-linux-amd64" => "branch-wrangler"
     end
   end
 
